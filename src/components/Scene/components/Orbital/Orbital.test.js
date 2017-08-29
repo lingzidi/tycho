eeb7e3a6487284af36c650a915e4384d115f25ed
jpshor <jpshor@gmail.com>
2017-08-29 11:06:20 -0400

    adds orbital test

---
+++ src/components/Scene/components/Orbital/Orbital.test.js
@@ -0,0 +1,31 @@
+import Orbital from './Orbital';
+import React from 'react';
+import {shallow} from 'enzyme';
+
+describe('Orbital Component', () => {
+  describe('updateScreenPositions()', () => {
+    it('should call the updateScreenPosition callback prop', () => {
+      const updateScreenPosition = jest.fn();
+      const position = {};
+      const rotation = {};
+
+      const component = shallow(<Orbital
+          updateScreenPosition={updateScreenPosition}
+          eclipticGroupRotation={rotation}
+          orbitalGroupRotation={rotation}
+          pathVertices={[]}
+          bodyPosition={position}
+          bodyRotation={rotation}
+          bodyRadius={1}
+          id="testOrbital"
+        />);
+
+      const orbital = component.instance();
+
+      orbital.onAnimationFrame();
+
+      expect(updateScreenPosition).toHaveBeenCalled();
+      expect(updateScreenPosition).toHaveBeenCalledTimes(1);
+    });
+  });
+});
