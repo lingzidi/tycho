bd81e2d15cf7c851476286a33b7560f3a3cff5dc
jpshor <jpshor@gmail.com>
2017-08-31 12:58:15 -0400

    esdoc for Math2 lib

--- src/services/Math2.js
+++ src/services/Math2.js
@@ -4,6 +4,14 @@ export default class Math2 {
 
   static TAU = Math.PI * 2;
   
+  /**
+   * Calculates the circumference of an ellipse.
+   * This uses Ramanujan's elliptical approximation formula.
+   *
+   * @param {Number} a - semimajor axis
+   * @param {Nubmer} b - semiminor axis
+   * @returns {Number} circumference of ellipse
+   */
   static ramanujan(a, b) {
     const p = 3 * b + a;
     const q = 3 * a + b;
@@ -12,22 +20,55 @@ export default class Math2 {
     return Math.PI * (Math.sqrt(p * q) - r);
   }
 
+  /**
+   * Returns one of two focii of an ellipse.
+   *
+   * @param {Number} x - semimajor axis
+   * @param {Number} y - semiminor axis
+   * @returns {Number} focus of ellipse
+   */
   static getFocus(x, y) {
     return Math.sqrt(Math.pow(x, 2) - Math.pow(y, 2));
   }
 
+  /**
+   * Converts the given degrees to radians.
+   *
+   * @param {Number} deg - number to convert, in degrees
+   * @returns {Number} result in radians
+   */
   static toRadians(deg) {
     return deg * Math.PI / 180;
   }
 
+  /**
+   * Converts the given radians to degrees.
+   *
+   * @param {Number} rad - number to convert, in radians
+   * @returns {Number} result in degrees
+   */
   static toDegrees(rad) {
     return (rad * 180) / Math.PI;
   }
 
+  /**
+   * Converts arcseconds to radians for the given time.
+   *
+   * @param {Number} time - UNIX timestamp
+   * @param {Number} rotation - rotation, in arcseconds
+   * @returns {Number} result in radians
+   */
   static arcSecToRad(time, rotation) {
     return this.toRadians(this.arcSecToDeg(time, rotation));
   }
  
+ /**
+  * Converts arcseconds to degrees for the given time.
+  *
+  * @param {Number} time - UNIX timestamp
+  * @param {Number} rotation - rotation, in arcseconds
+  * @returns {Number} result in degrees
+  */
   static arcSecToDeg(time, rotation) {
     return time * (rotation / 3600) % 360;
   }
