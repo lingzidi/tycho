b70761868c55132d5d08d7634166aa4559b1f642
Josh Shor <jpshor@gmail.com>
2018-03-09 16:37:21 -0500

    fixes redux devtools connector

--- src/store/__tests__/index.test.js
+++ src/store/__tests__/index.test.js
@@ -2,17 +2,6 @@ import store from '../';
 
 describe('Store', () => {
     it('should return an object', () => {
-        expect(typeof store({})).toBe('object');  
-    });
-
-    it('should connect to the redux devtools, if available', () => {
-        const key = '__REDUX_DEVTOOLS_EXTENSION__';
-        window[key] = jest.fn();
-        const spy = jest.spyOn(window, key);
-
-        store({});
-
-        expect(spy).toHaveBeenCalled();
-        expect(spy).toHaveBeenCalledTimes(1);
+        expect(typeof store({})).toBe('object');
     });
 });
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default (initialState) => {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        composeEnhancers(applyMiddleware(thunk)),
     );
 }
